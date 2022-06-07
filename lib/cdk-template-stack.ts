e4cb40e6fda69fb83e3a9695758421c4eff21e4d
小暮 伸哉 <A20053@works.local>
2022-06-07 22:16:10 +0900

    VPCの作成、publicサブネットの作成

--- lib/cdk-template-stack.ts
+++ lib/cdk-template-stack.ts
@@ -1,10 +1,30 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 export class CdkTemplateStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'TheVPC', {
+      cidr: '10.0.0.0/21',
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          
+          subnetType: ec2.SubnetType.PUBLIC,
+          name: 'SubnetForWebServer',
+          cidrMask: 24,
+        },
+        {
+          cidrMask: 24,
+          name: 'SubnetForDatabaseServer',
+          subnetType: ec2.SubnetType.ISOLATED,
+          reserved: true
+        }
+      ],
+    });
+//private-subnetも作成できるように。
+    
   }
 }
